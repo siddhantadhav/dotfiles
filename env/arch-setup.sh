088c9f817d14297925598348b8aabf61d50c9a6c
siddhant <isiddhantadhav@gmail.com>
2025-06-16 04:32:04 +0530

    wofi, nvim, tmux, waybar config backed up

--- env/arch-setup.sh
+++ env/arch-setup.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
 
+# update packages
 sudo pacman -Syu
+
+# install stow
 sudo pacman -S stow
+
+# install nvim
 sudo pacman -S nvim
+
+# install waybar
 sudo pacman -S waybar
+
+# install tmux
 sudo pacman -S tmux
+mkdir -p ~/.config/tmux/plugins
+git clone git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins
+
+# install wofi
+sudo pacman -S wofi
